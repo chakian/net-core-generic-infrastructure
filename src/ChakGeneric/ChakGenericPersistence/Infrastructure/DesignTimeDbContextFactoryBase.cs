886164450621751f41a9990fa4d26aa5555b650c
Cagdas Korkut <cagdaskorkut@gmail.com>
2018-11-24 14:25:30 +0300

    added Persistence project

---
+++ src/ChakGeneric/ChakGenericPersistence/Infrastructure/DesignTimeDbContextFactoryBase.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace ChakGenericPersistence.Infrastructure
+{
+    public abstract class DesignTimeDbContextFactoryBase<TContext> :
+        IDesignTimeDbContextFactory<TContext> where TContext : DbContext
+    {
+        protected readonly string ConnectionStringName;
+        protected readonly string AspNetCoreEnvironment;
+        protected readonly string WebProjectName;
+
+        public DesignTimeDbContextFactoryBase(string connectionStringName,
+            string aspNetCoreEnvironment,
+            string webProjectName)
+        {
+            ConnectionStringName = connectionStringName;
+            AspNetCoreEnvironment = aspNetCoreEnvironment;
+            WebProjectName = webProjectName;
+        }
+
+        public TContext CreateDbContext(string[] args)
+        {
+            var basePath = Directory.GetCurrentDirectory() + string.Format("{0}..{0}{1}", Path.DirectorySeparatorChar, WebProjectName);
+            return Create(basePath, Environment.GetEnvironmentVariable(AspNetCoreEnvironment));
+        }
+
+        protected abstract TContext CreateNewInstance(DbContextOptions<TContext> options);
+
+        private TContext Create(string basePath, string environmentName)
+        {
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.Local.json", optional: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var connectionString = configuration.GetConnectionString(ConnectionStringName);
+
+            return Create(connectionString);
+        }
+
+        private TContext Create(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentException($"Connection string '{ConnectionStringName}' is null or empty.", nameof(connectionString));
+            }
+
+            Console.WriteLine($"DesignTimeDbContextFactoryBase.Create(string): Connection string: '{connectionString}'.");
+
+            var optionsBuilder = new DbContextOptionsBuilder<TContext>();
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return CreateNewInstance(optionsBuilder.Options);
+        }
+    }
+}
