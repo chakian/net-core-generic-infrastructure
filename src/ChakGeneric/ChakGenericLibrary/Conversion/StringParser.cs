b834f849a1b6d5e9c461f15123a3e9f4aad18852
Cagdas Korkut <cagdaskorkut@gmail.com>
2018-11-24 14:35:17 +0300

    added Common Library project

---
+++ src/ChakGeneric/ChakGenericLibrary/Conversion/StringParser.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace ChakGenericLibrary.Conversion
+{
+    public class StringParser
+    {
+        /// <summary>
+        /// Converts a byte array to a string with UTF8 format
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string ByteArrayToString(byte[] input)
+        {
+            return Encoding.UTF8.GetString(input);
+        }
+
+        /// <summary>
+        /// Converts a string to a byte array with UTF8 format
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static byte[] StringToByteArray(string input)
+        {
+            return Encoding.UTF8.GetBytes(input);
+        }
+    }
+}
---
+++ src/ChakGeneric/ChakGenericLibrary/Encryption/HashHelper.cs
@@ -0,0 +1,28 @@
+﻿using ChakGenericLibrary.Conversion;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ChakGenericLibrary.Encryption
+{
+    public class HashHelper
+    {
+        public static string GetHash(string data)
+        {
+            SHA512 sha = SHA512.Create();
+
+            //convert the input text to array of bytes
+            byte[] hashData = sha.ComputeHash(StringParser.StringToByteArray(data));
+
+            StringBuilder returnValue = new StringBuilder();
+
+            //loop for each byte and add it to StringBuilder
+            for (int i = 0; i < hashData.Length; i++)
+            {
+                returnValue.Append(hashData[i].ToString());
+            }
+
+            // return hexadecimal string
+            return returnValue.ToString();
+        }
+    }
+}
